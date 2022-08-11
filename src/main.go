6a2018c2704515f404ee815a6c39b1220f97f9eb
kynmh69 <hiroki.kayanuma@gmail.com>
2022-08-11 15:48:46 +0900

    initial dev #1  add log

--- src/main.go
+++ src/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
 	"simple_add_page/src/notion/page"
 	"simple_add_page/src/util"
 )
 
 func main() {
+	logger := log.Default()
 
 	content := util.ReadPhotoIdFile("")
 	id_list := util.SplitLines(*content)
@@ -14,6 +16,7 @@ func main() {
 
 	for _, v := range id_list {
 		page.Create(v, tag_name)
+		logger.Println("Created. id:", v)
 	}
 
 }
