08728ee02ab9620f0b934cd1aa32d206108a0289
kynmh69 <hiroki.kayanuma@gmail.com>
2022-08-11 11:26:30 +0900

    initial dev #1  add config

--- src/util/const.go
+++ src/util/const.go
@@ -1,4 +1,3 @@
 package util
 
 const TYPE_MULTI_SELECT = "multi_select"
-const DATABASE_ID = "ed3815aeb0dc447eabfc54f203c180fd"
--- src/util/file.go
+++ src/util/file.go
@@ -1,3 +1,16 @@
-func readConfigFile() {
+package util
+
+func ReadConfigFile(config_file_path string) {
+	if config_file_path != nil || conconfig_file_path != "" {
+		
+	} else {
+		config_file_path = "./config/api.yml"
+	}
+	
+}
+
+type ApiSettings struct {
+	ApiSecret string
+	DatabaseID string
 
 }
